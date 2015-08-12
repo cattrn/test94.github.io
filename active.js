735e18eb7b527f988ee76be36a159988cc457335
test94 <caterina.turnbull@gmail.com>
2015-08-12 17:23:20 +1000

    Update active.js

--- active.js
+++ active.js
@@ -2,7 +2,7 @@ var url = window.location.pathname;
 var filename = url.substring(url.lastIndexOf('/')+1);
 
 $(function() {
-  $(if(filename = event.html){
+  $(if(filename = 'event.html'){
     $("a:contains('Events')").parent().addClass('active')
   })
 })
