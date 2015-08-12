87b0566a7448d47229dc9fe419c64b82d361d32e
test94 <caterina.turnbull@gmail.com>
2015-08-12 17:27:54 +1000

    Update active.js

--- active.js
+++ active.js
@@ -1,8 +1,18 @@
-var url = window.location.pathname;
-var filename = url.substring(url.lastIndexOf('/')+1);
+function getFileName() {
+//this gets the full url
+var url = document.location.href;
+//this removes the anchor at the end, if there is one
+url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
+//this removes the query after the file name, if there is one
+url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
+//this removes everything before the last slash in the path
+url = url.substring(url.lastIndexOf("/") + 1, url.length);
+//return
+return url;
+}
 
 $(function() {
-  $(if(filename = 'event.html'){
+  $(if(url = 'events.html'){
     $("a:contains('Events')").parent().addClass('active')
   })
 })
